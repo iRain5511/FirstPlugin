8113907f9f0e91f88ef087d6ad91ead7d91945a0
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 13:20:44 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -19,16 +19,17 @@ class Main extends PluginBase{
     }
         
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        switch($command->getName()){
-            $this->getServer()->getCommandMap()->dispatch($player, "tttt"); 
-            $sender->sendMessage("Item is now in your Inventory!");
-            $name = $sender->getName();
-            $sender->addTitle("Hi " . $name);
-            $item = Item::get(1,0,64);
-            $inv = $sender->genInventory();
-            $inv->addItem($item);
-            return true;
-		}
-	}
+	    $player->$sender->getPlayer();
+	    switch($cmd_>getName()){
+		case "test":
+                $this->getServer()->getCommandMap()->dispatch($player, "tttt"); 
+            	$sender->sendMessage("Item is now in your Inventory!");
+            	$name = $sender->getName();
+            	$sender->addTitle("Hi " . $name);
+            	$item = Item::get(1,0,64);
+            	$inv = $sender->genInventory();
+            	$inv->addItem($item);
+            	return true;
+	    }
+      }
 }
-
