f4be7fa74fc705d18236216908d3e1fe31c48a7f
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 12:55:15 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -21,7 +21,7 @@ class Main extends PluginBase{
     }
         
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        switch($command->getName())
+        switch($command->getName()){
             case "ok":
             $sender->sendMessage("Item is now in your Inventory!");
             $name = $sender->getName();
