d83f80b7e09d71d84c6c33b3f669c5c5e536dba6
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 14:17:04 -0700

    Add files via upload

---
+++ src/iRain/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace iRain;
+
+use pocketmine\plugin\PluginBase
+use pocketmine\event\Listener
+use pocketmine\Player
+use pocketmine\Server
+use pocketmine\command\CommandSender
+use pocketmine\command\Command
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase implements Listener {
+
+    public $prefix = C::YELLOW."TEST".C::DARK_GRAY." >"xC::WHITE."";
+
+    public function onLoad(){
+        $this->getLogger()->info($this->prefix."IDK"
+    }
+
+    public function onEnable(){
+        $this->getLogger()->info($this->prefix."IDK");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if($cmd->getName() -- "test"){
+            $sender->sendMessage($this->prefix."Hello")
+        }
+    }
+
+    public function onDisable()
+        $this->getLogger()->info($this->prefix. "Oml");
+    }
+}
