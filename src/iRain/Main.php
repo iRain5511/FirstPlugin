a0402fad2ac2d27e156b0f0656eb618ff14f8a7f
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 09:41:37 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -2,19 +2,36 @@
 
 namespace iRain;
 
+use pocketmine\Server;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
 
 class Main extends PluginBase{
-
-        public function onLoad(){
-                $this->getLogger()->info("onLoad() has been called!");
+        
+    public function onEnable (){
+        $this->getLogger()->Info("This Plugin Has Been Enabled");
+    }
+        
+    public function onDisable(){
+        $this->getLogger()->Info("This Plugin Has Been Disabled");
+    }
+        
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+        if($sender instanceof Player)(
+            switch($cmd->getName())(
+                case "iraindrop":
+                $sender->addTitle("Oof");
+                $item = Item::get(278,0,1);
+                $inv = $sender->getInventory();
+                $item->setCustomName("Okie");
+                $inv->addItem($item);
+            }else{
+                $sender->sendMessage("Please Use This Command In Game Please");
+                return true;
+            }
         }
-
-        public function onEnable(){
-                $this->getLogger()->info("onEnable() has been called!");
-        }
-
-        public function onDisable(){
-                $this->getLogger()->info("onDisable() has been called!");
-        }
-}
+    }
+}                      
