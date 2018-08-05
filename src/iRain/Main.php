d928f245b9599cabdb9efc46437fcd0d9d1cab8d
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 13:39:24 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -1,35 +1,28 @@
 <?php
-namespace iRain;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
-use pocketmine\item\Item;
 
+/*...*/
+
+declare(strict_types=1);
+
+namespace PluginTutorial;
+
+use pocketmine\plugin\PluginBase
+	
 class Main extends PluginBase{
-        
-    public function onEnable (){
-        $this->getLogger()->Info("This Plugin Has Been Enabled");
-    }
-        
-    public function onDisable(){
-        $this->getLogger()->Info("This Plugin Has Been Disabled");        
-    }
-        
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-	    $player->$sender->getPlayer();
-	    switch($cmd_>getName()){
-		case "test":
-                $this->getServer()->getCommandMap()->dispatch($player, "tttt"); 
-            	$sender->sendMessage("Item is now in your Inventory!");
-            	$name = $sender->getName();
-            	$sender->addTitle("Hi " . $name);
-            	$item = Item::get(1,0,64);
-            	$inv = $sender->genInventory();
-            	$inv->addItem($item);
-            	return true;
-	    }
-      }
+	
+   public function onEnable(){ : void{
+	   $this->getLogger()->info("Plugin has been enabled");
+   }
+			      
+   public function onDisable() : void{
+	   $this->getLogger()->info("Plugin has been disabled");
+   }   
+	   
+   public function onCommand(CommandSender $sender, Command $command, string $label, array $args : bool{
+       switch(command){
+	       case "Hello":
+	           $sender->sendMessage("Hello to you too!");
+	           break;
+        }
+    }	   	   
 }
