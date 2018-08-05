4721d3c48f4768ce448e5deb812467c94ee4b4b9
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 12:37:38 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -19,17 +19,6 @@ class Main extends PluginBase{
     public function onDisable(){
         $this->getLogger()->Info("This Plugin Has Been Disabled");
     }
-        
-    public function onCommand(CommandSender $sender, pocketmine\command\Command $command, string $label, array $args): bool{
-        if($sender instanceof Player){
-            switch($command->getName()){
-                case "iraindrop":
-                $sender->addTitle("Oof");
-                $item = Item::get(278,0,1);
-                $inv = $sender->getInventory();
-                $item->setCustomName("Okie");
-                $item->setLore("Okie");
-                $inv->addItem($item);
             }
                 
         }
