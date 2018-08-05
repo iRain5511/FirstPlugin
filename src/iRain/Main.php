e79a7acfad2b8173746ebdb73f05f221075b1065
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-05 09:02:18 -0700

    Add files via upload

---
+++ src/iRain/Main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace iRain;
+
+use pocketmine\plugin\PluginBase;
+
+class iRain extends PluginBase{
+
+        public function onLoad(){
+                $this->getLogger()->info("onLoad() has been called!");
+        }
+
+        public function onEnable(){
+                $this->getLogger()->info("onEnable() has been called!");
+        }
+
+        public function onDisable(){
+                $this->getLogger()->info("onDisable() has been called!");
+        }
+}
