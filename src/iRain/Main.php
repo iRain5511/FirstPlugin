b98b7ba21d7ebf7a39a0311a00454a39391ee975
iRain5511 <41877521+iRain5511@users.noreply.github.com>
2018-08-06 13:39:05 -0700

    Update Main.php

--- src/iRain/Main.php
+++ src/iRain/Main.php
@@ -8,25 +8,50 @@ use pocketmine\Server;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
 
 class Main extends PluginBase implements Listener {
-    public $prefix = TextFormat::YELLOW."TEST".TextFormat::DARK_GRAY."";
+    public const PREFIX = TextFormat::YELLOW . "Welcome!" . TextFormat::DARK_GRAY. " ";
 
-    public function onLoad(){
-        $this->getLogger()->info($this->prefix."IDK");
-    }
-
-    public function onEnable(){
-        $this->getLogger()->info($this->prefix."IDK");
-    }
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        if($cmd->getName() == "test"){
-            $sender->sendMessage($this->prefix."Hello");
+    public function onLoad()  : void {
+        $int = rand(1, 3);
+        switch ($int) {
+        case 1:
+            $this->getLogger()->info(Main::PREFIX . "CODING");
+            break;
+        case 2:
+            $this->getLogger()->info(Main::PREFIX . "IS");
+            break;
+        case 3:
+            $this->getLogger()->info(Main::PREFIX . "FUN");
+            break;
         }
     }
-    public function onDisable(){
-        $this->getLogger()->info($this->prefix."Oml");
+
+    public function onEnable() : void {
+        $this->getLogger()->info(Main::PREFIX . "IDK");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) : bool
+    {
+        $time = time();
+        $date = date('Y-m-d');
+        if ($sender instanceof Player) {
+            switch ($cmd->getName()) {
+                case "iraindrop":
+                    $sender->sendMessage(Main::PREFIX . "This plugin is made by @iRainDrop_ (That is my Twitter)");
+                    $sender->addTitle(TextFormat::RED . TextFormat::BOLD . "OOF");
+                    $player->getInventory()->setItem(5, Item::get(264,0,1));
+                    return true;
+                case "cdate":
+                    $sender->sendMessage(Main::PREFIX . "The date is " . $date);
+                    return true;
+            }
+        }
+    }
+
+
+    public function onDisable() : void {
+        $this->getLogger()->info(Main::PREFIX . "Oml");
     }
 }
-
